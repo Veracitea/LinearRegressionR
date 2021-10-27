1049a39047088e99869c4e6605dc2e96341a7586
Veracitea <58912917+Veracitea@users.noreply.github.com>
2021-10-28 01:57:19 +0800

    Update codes_v1.0.R

--- codes_v1.0.R
+++ codes_v1.0.R
@@ -14,3 +14,24 @@ cor(aadt)
 mlr<- lm(Y~ X1+X2+X3+X4, data = aadt)
 summary(mlr)
 anova(mlr)
+
+#Model 0: Normality checking.
+qqnorm(residuals(mlr),ylab='Residuals')
+qqline(residuals(mlr))
+
+#Model 0: Draw some plots of residuals.
+par(mfrow=c(1,3))
+plot(residuals(mlr),ylab='Residuals',xlab='Time')
+plot(residuals(mlr),fitted(mlr),ylab='Residuals',xlab='Fitted values')
+plot(residuals(mlr),aadt_main$V1,ylab='Residuals',xlab='Predictor variable')
+par(mfrow=c(1,1)) 
+par(mfrow=c(1,4))
+plot(residuals(mlr),aadt_main$V2,ylab='Residuals',xlab='X1')
+plot(residuals(mlr),aadt_main$V3,ylab='Residuals',xlab='X2')
+plot(residuals(mlr),aadt_main$V4,ylab='Residuals',xlab='X3')
+plot(residuals(mlr),aadt_main$V5,ylab='Residuals',xlab='X4')
+par(mfrow=c(1,1)) 
+
+#Test for removal of X3
+mlr1<- lm(Y~ X1+X2+X4, data = aadt)
+anova(mlr1,mlr)
